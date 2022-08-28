c4ae762e40b023b71fdac497263bb9807a547b9f
pgm-bramvand2 <bram.vandenbussche@studiohyperdrive.be>
2022-08-28 11:54:03 +0200

    feature(character-overview): Add current hp functionality.

--- src/app/shared/components/character-bar/character-bar.component.ts
+++ src/app/shared/components/character-bar/character-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 import { FirestoreService } from '../../services/firebase/firestore/firestore.service';
 import { LocalstorageService } from '../../services/localstorage/localstorage.service';
 import { ScoreCalculatorService } from '../../services/score-calculator/score-calculator.service';
@@ -20,12 +21,14 @@ export class CharacterBarComponent implements OnInit {
     private localStorageService: LocalstorageService,
     private firestoreService: FirestoreService,
     private scoreCalculatorService: ScoreCalculatorService,
+    private alertController: AlertController
   ) { }
 
   ngOnInit() {
     this.character = this.localStorageService.getStorageItem('character');
     this.setCurrentHp();
     this.setCharacter();
+    console.log(this.character);
   }
 
   updateCharacter(character) {
@@ -34,15 +37,30 @@ export class CharacterBarComponent implements OnInit {
   }
 
 
-  setCurrentHp() {
+  setCurrentHp(newHp: number = null) {
+    let hp: number;
+    console.log(this.character.currentHp);
+
     if(!this.character.currentHp) {
-      this.firestoreService.updateCharacter(this.character.id, {currentHp: this.character.totalHp});
-      this.updateCharacter({
-        ...this.character,
-        currentHp: this.character.totalHp
-      });
+      hp = this.character.totalHp;
+      console.log('current if-> ', hp);
     }
+
+    hp = this.character.currentHp;
+
+    if(newHp) {
+      console.log('newhp if-> ', hp);
+      hp = newHp;
+    }
+    console.log('after if-> ', hp);
+
+    this.firestoreService.updateCharacter(this.character.id, {currentHp: hp});
+    this.updateCharacter({
+      ...this.character,
+      currentHp: hp
+    });
   }
+
 
   setCharacter() {
     if(!this.character.initiative) {
@@ -71,4 +89,42 @@ export class CharacterBarComponent implements OnInit {
     this.proficiencyBonus= this.scoreCalculatorService.addPlusSign(this.character.proficiencyBonus);
   }
 
+  async hpAlert() {
+    const alert = await this.alertController.create({
+      header: 'Set your current Hitpoints',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+              console.log('Confirm Cancel');
+          }
+      },
+      {
+          text: 'Ok',
+          handler: (alertData) => { //takes the data
+              this.setCurrentHp(alertData.newHp);
+          }
+      }
+      ],
+      inputs: [
+        {
+          name: 'newHp',
+          type: 'number',
+          placeholder: 'HP',
+          min: 0,
+          max: this.character.totalHp,
+          value: this.character.currentHp
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+
+  async onClickHp() {
+   this.hpAlert();
+  }
 }
+
